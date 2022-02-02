9ccd33b0f634f4395f9651b63def502ed48ce8cc
gtgtgl <ykr20141101@outlook.jp>
2022-02-02 10:10:57 +0900

    カスタム投稿タイプの整理

--- inc/custom_post_type.php
+++ inc/custom_post_type.php
@@ -20,98 +20,8 @@ function hamahome_unregister_taxonomies()
  
 add_action('init', 'hamahome_unregister_taxonomies');
 
-// 「投稿」を「お知らせ」に変更する
-function hamahome_change_menulabel() {
-	global $menu;
-	global $submenu;
-	$name = 'お知らせ';
-	$menu[5][0] = $name;
-	$submenu['edit.php'][5][0] = $name.'一覧';
-	$submenu['edit.php'][10][0] = '新しい'.$name;
-}
-function hamahome_change_objectlabel() {
-	global $wp_post_types;
-	$name = 'お知らせ';
-	$labels = &$wp_post_types['post']->labels;
-	$labels->name = $name;
-	$labels->singular_name = $name;
-	$labels->add_new = _x('追加', $name);
-	$labels->add_new_item = $name.'の新規追加';
-	$labels->edit_item = $name.'の編集';
-	$labels->new_item = '新規'.$name;
-	$labels->view_item = $name.'を表示';
-	$labels->search_items = $name.'を検索';
-	$labels->not_found = $name.'が見つかりませんでした';
-	$labels->not_found_in_trash = 'ゴミ箱に'.$name.'は見つかりませんでした';
-}
-add_action( 'init', 'hamahome_change_objectlabel' );
-add_action( 'admin_menu', 'hamahome_change_menulabel' );
-
 // let's create the function for the custom type
 function custom_post_example() {
-	// creating (registering) the custom type
-	register_post_type( 'president_blog', /* (http://codex.wordpress.org/Function_Reference/register_post_type) */
-		// let's now add all the options for this post type
-		array( 'labels' => array(
-			'name' => __( '社長ブログ', 'hamahome' ), /* This is the Title of the Group */
-			'singular_name' => __( '社長ブログ', 'hamahome' ), /* This is the individual type */
-			'all_items' => __( '社長ブログ一覧', 'hamahome' ), /* the all items menu item */
-			'add_new' => __( 'ブログを投稿する', 'hamahome' ), /* The add new menu item */
-			'add_new_item' => __( '新しいブログを投稿する', 'hamahome' ), /* Add New Display Title */
-			'edit' => __( '編集', 'hamahome' ), /* Edit Dialog */
-			'edit_item' => __( '編集', 'hamahome' ), /* Edit Display Title */
-			'new_item' => __( '新しい社長ブログ', 'hamahome' ), /* New Display Title */
-			'view_item' => __( '社長ブログを見る', 'hamahome' ), /* View Display Title */
-			'search_items' => __( '社長ブログを検索', 'hamahome' ), /* Search Custom Type Title */
-			'not_found' =>  __( 'まだ社長ブログがありません', 'hamahome' ), /* This displays if there are no entries yet */
-			'parent_item_colon' => ''
-			), /* end of arrays */
-			'public' => true,
-			'publicly_queryable' => true,
-			'exclude_from_search' => false,
-			'show_ui' => true,
-			'query_var' => true,
-			'menu_position' => 8, /* this is what order you want it to appear in on the left hand side menu */
-			'menu_icon' => 'dashicons-edit', /* the icon for the custom post type menu */
-			'has_archive' => true, /* you can rename the slug here */
-			'capability_type' => 'post',
-			'hierarchical' => true,
-			/* the next one is important, it tells what's enabled in the post editor */
-			'supports' => array( 'title', 'editor', 'thumbnail', 'author', 'excerpt', 'sticky')
-		) /* end of options */
-	); /* end of register post type */
-    
-	// creating (registering) the custom type
-	register_post_type( 'staff_blog', /* (http://codex.wordpress.org/Function_Reference/register_post_type) */
-		// let's now add all the options for this post type
-		array( 'labels' => array(
-			'name' => __( 'スタッフブログ', 'hamahome' ), /* This is the Title of the Group */
-			'singular_name' => __( 'スタッフブログ', 'hamahome' ), /* This is the individual type */
-			'all_items' => __( 'スタッフブログ一覧', 'hamahome' ), /* the all items menu item */
-			'add_new' => __( 'ブログを投稿する', 'hamahome' ), /* The add new menu item */
-			'add_new_item' => __( '新しいブログを投稿する', 'hamahome' ), /* Add New Display Title */
-			'edit' => __( '編集', 'hamahome' ), /* Edit Dialog */
-			'edit_item' => __( '編集', 'hamahome' ), /* Edit Display Title */
-			'new_item' => __( '新しいスタッフブログ', 'hamahome' ), /* New Display Title */
-			'view_item' => __( 'スタッフブログを見る', 'hamahome' ), /* View Display Title */
-			'search_items' => __( 'スタッフブログを検索', 'hamahome' ), /* Search Custom Type Title */
-			'not_found' =>  __( 'まだスタッフブログがありません', 'hamahome' ), /* This displays if there are no entries yet */
-			'parent_item_colon' => ''
-			), /* end of arrays */
-			'public' => true,
-			'publicly_queryable' => true,
-			'exclude_from_search' => false,
-			'show_ui' => true,
-			'query_var' => true,
-			'menu_position' => 8, /* this is what order you want it to appear in on the left hand side menu */
-			'menu_icon' => 'dashicons-edit', /* the icon for the custom post type menu */
-			'has_archive' => true, /* you can rename the slug here */
-			'capability_type' => 'post',
-			'hierarchical' => true,
-			/* the next one is important, it tells what's enabled in the post editor */
-			'supports' => array( 'title', 'editor', 'thumbnail', 'author', 'excerpt', 'sticky')
-		) /* end of options */
-	); /* end of register post type */
 
 	// creating (registering) the custom type
 	register_post_type( 'construction', /* (http://codex.wordpress.org/Function_Reference/register_post_type) */
@@ -146,48 +56,6 @@ function custom_post_example() {
 	); /* end of register post type */
 }
 	add_action( 'init', 'custom_post_example');
-
-	register_taxonomy( 'president_blog_cat',
-		array('president_blog'), /* if you change the name of register_post_type( 'custom_type', then you have to change this */
-		array('hierarchical' => true,     /* if this is true, it acts like categories */
-			'labels' => array(
-				'name' => __( 'カテゴリー', 'hamahome' ), /* name of the custom taxonomy */
-				'singular_name' => __( 'カテゴリー', 'hamahome' ), /* single taxonomy name */
-				'all_items' => __( 'カテゴリー一覧', 'hamahome' ), /* all title for taxonomies */
-				'parent_item' => __( '親カテゴリー', 'hamahome' ), /* parent title for taxonomy */
-				'parent_item_colon' => __( '親カテゴリー：', 'hamahome' ), /* parent taxonomy title */
-				'edit_item' => __( '編集', 'hamahome' ), /* edit custom taxonomy title */
-				'update_item' => __( '更新', 'hamahome' ), /* update title for taxonomy */
-				'add_new_item' => __( 'カテゴリーを追加する', 'hamahome' ), /* add new title for taxonomy */
-				'new_item_name' => __( 'カテゴリーの名前', 'hamahome' ) /* name title for taxonomy */
-			),
-			'show_admin_column' => true,
-			'show_ui' => true,
-			'query_var' => true,
-      'has_archive' => true,  //trueにすると投稿した記事の一覧ページを作成することができる
-		)
-	);
-
-	register_taxonomy( 'staff_blog_cat',
-		array('staff_blog'), /* if you change the name of register_post_type( 'custom_type', then you have to change this */
-		array('hierarchical' => true,     /* if this is true, it acts like categories */
-			'labels' => array(
-				'name' => __( 'カテゴリー', 'hamahome' ), /* name of the custom taxonomy */
-				'singular_name' => __( 'カテゴリー', 'hamahome' ), /* single taxonomy name */
-				'all_items' => __( 'カテゴリー一覧', 'hamahome' ), /* all title for taxonomies */
-				'parent_item' => __( '親カテゴリー', 'hamahome' ), /* parent title for taxonomy */
-				'parent_item_colon' => __( '親カテゴリー：', 'hamahome' ), /* parent taxonomy title */
-				'edit_item' => __( '編集', 'hamahome' ), /* edit custom taxonomy title */
-				'update_item' => __( '更新', 'hamahome' ), /* update title for taxonomy */
-				'add_new_item' => __( 'カテゴリーを追加する', 'hamahome' ), /* add new title for taxonomy */
-				'new_item_name' => __( 'カテゴリーの名前', 'hamahome' ) /* name title for taxonomy */
-			),
-			'show_admin_column' => true,
-			'show_ui' => true,
-			'query_var' => true,
-      'has_archive' => true,  //trueにすると投稿した記事の一覧ページを作成することができる
-		)
-	);
 
 	register_taxonomy( 'construction_cat',
 		array('construction'), /* if you change the name of register_post_type( 'custom_type', then you have to change this */
--- inc/front_page/_news.php
+++ inc/front_page/_news.php
@@ -14,12 +14,17 @@
 					<div id="content_news" class="panel content_news is-show">
 						<div class="panel_header">
 							<p class="title">お知らせ</p>
-							<a href="<?php echo get_post_type_archive_link( 'post' ); ?>" class="btn_list">一覧へ<img src="<?php echo get_template_directory_uri(); ?>/img/dot_icon.svg" alt="一覧へ"></a>
+							<?php 
+								$category_id_news = get_cat_ID( 'お知らせ' );
+								$category_link_news = get_category_link( $category_id_news );
+								 ?>
+							<a href="<?php echo esc_url( $category_link_news ); ?>" class="btn_list">一覧へ<img src="<?php echo get_template_directory_uri(); ?>/img/dot_icon.svg" alt="一覧へ"></a>
 						</div>
 						
 						<?php
 							$args = array(
 								'post_type' => 'post',
+								'category_name'    => 'news',
 								'posts_per_page' => 3 //表示件数（-1で全ての記事を表示）
 							);
 							$the_query = get_posts( $args );
@@ -40,12 +45,17 @@
 							<p class="title">
 							<img src="<?php echo get_template_directory_uri(); ?>/img/note.svg" alt="社長ブログ">社長ブログ
 							</p>
-							<a href="<?php echo get_post_type_archive_link( 'president_blog' ); ?>" class="btn_list">一覧へ<img src="<?php echo get_template_directory_uri(); ?>/img/dot_icon.svg" alt="一覧へ"></a>
+							<?php 
+								$category_id_president = get_cat_ID( '社長ブログ' );
+								$category_link_president = get_category_link( $category_id_president );
+								 ?>
+							<a href="<?php echo esc_url( $category_link_president ); ?>" class="btn_list">一覧へ<img src="<?php echo get_template_directory_uri(); ?>/img/dot_icon.svg" alt="一覧へ"></a>
 						</div>
 
 						<?php
 							$args = array(
-								'post_type' => 'president_blog',
+								'post_type' => 'post',
+								'category_name'    => 'president',
 								'posts_per_page' => 3 //表示件数（-1で全ての記事を表示）
 							);
 							$the_query = get_posts( $args );
@@ -67,13 +77,18 @@
 							<p class="title">
 							<img src="<?php echo get_template_directory_uri(); ?>/img/note.svg" alt="スタッフブログ">スタッフブログ
 							</p>
-							<a href="<?php echo get_post_type_archive_link( 'staff_blog' ); ?>" class="btn_list">一覧へ<img src="<?php echo get_template_directory_uri(); ?>/img/dot_icon.svg" alt="一覧へ"></a>
+							<?php 
+								$category_id_staff = get_cat_ID( 'スタッフブログ' );
+								$category_link_staff = get_category_link( $category_id_staff );
+								 ?>
+							<a href="<?php echo esc_url( $category_link_staff ); ?>" class="btn_list">一覧へ<img src="<?php echo get_template_directory_uri(); ?>/img/dot_icon.svg" alt="一覧へ"></a>
 						</div>
 						
 
 					<?php
 						$args = array(
-							'post_type' => 'staff_blog',
+							'post_type' => 'post',
+							'category_name'    => 'staff',
 							'posts_per_page' => 3 //表示件数（-1で全ての記事を表示）
 						);
 						$the_query = get_posts( $args );
