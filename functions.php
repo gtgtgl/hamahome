81f48f70ec99a8cc5961eb34644df33645fbac29
Taigo Ito <webmaster@qwel.design>
2022-02-09 10:25:45 +0900

    サイドバー内ウィジェットのスタイルを調整

--- functions.php
+++ functions.php
@@ -125,8 +125,8 @@ function hamahome_widgets_init() {
 			'name'          => esc_html__( 'Sidebar', 'hamahome' ),
 			'id'            => 'sidebar-1',
 			'description'   => esc_html__( 'Add widgets here.', 'hamahome' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</aside>',
 			'before_title'  => '<h2 class="widget-title">',
 			'after_title'   => '</h2>',
 		)
