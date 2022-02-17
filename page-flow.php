a7f87ec1e09c3852bce47d9633e8a565b6961d9d
gtgtgl <ykr20141101@outlook.jp>
2022-02-17 13:24:13 +0900

    家づくりの流れページ更新

--- page-flow.php
+++ page-flow.php
@@ -19,14 +19,14 @@ $slug = $page->post_name;
 	get_template_part( 'inc/_breadcrumb' );
 	?>
 
-    <section>
+    <section id="flow">
         <div class="container">
 
             <p class="left-center">
                 一般的な家づくりの流れです。大体の目安ですので、お客様によって多少異なります。
             </p>
 
-            <div class="flow_wrap">
+            <div class="flow_wrap mt60">
 
             <?php 
 
@@ -74,7 +74,7 @@ $slug = $page->post_name;
                 <div class="flow_card">
                     <div class="icon">
                         <div class="icon_inner">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/flow/icon<?php echo $key; ?>.svg" alt="<?php echo $item['title']; ?>" width="80">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/flow/icon<?php echo $key; ?>.svg" alt="<?php echo $item['title']; ?>">
                         </div>
                     </div>
                     <div class="text">
@@ -104,16 +104,142 @@ $slug = $page->post_name;
 
             $flow_items = array(
                 '09' => array(
-                    'title' => 'ヒアリング・プランニング',
-                    'content' => '大切な家づくりの最初の作業です。満足のいくまでじっくりと話し合いをします。気に入ったデザインなどあれば雑誌の切り抜きなどお持ちいただくのも良い方法です。'
+                    'title' => '基礎工事開始',
+                    'content' => 'ハマホームの基礎は安心の「ベタ基礎」。地震、地盤沈下にも強く安心です。'
                 ),
                 '10' => array(
-                    'title' => '設計図の制作',
+                    'title' => '鉄筋検査（瑕疵担保）',
                     'content' => '設計図を確認。変更はこの時点で。後になると変更できにくくなる場合も。'
                 ),
                 '11' => array(
-                    'title' => 'お見積り書の作成',
-                    'content' => 'そろそろ借り入れ先の選定をします。建て替えの場合は仮住まい探しもお早めに。'
+                    'title' => '詳細打ち合わせ',
+                    'content' => '一番最初に決定しなければならないのは屋根の色です。その他、急ぐ順番にサッシ、外壁、雨どい、外周りの塗装部、キッチン、バス、トイレなど機器や設備、色などを決めます。できるだけショールームで実物を確認して決定される方が良いでしょう。'
+                )
+                );
+             
+                foreach ($flow_items as $key => $item) {
+
+                ?>
+
+                <div class="flow_card">
+                    <div class="icon">
+                        <div class="icon_inner">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/flow/icon<?php echo $key; ?>.svg" alt="<?php echo $item['title']; ?>" width="80">
+                        </div>
+                    </div>
+                    <div class="text">
+                        <p class="title">
+                            <span class="number"><?php echo $key; ?></span>
+                            <?php echo $item['title']; 
+                            if(array_key_exists('orange', $item)) :
+                            ?>
+                            <span class="orange"><?php echo $item['orange']; ?></span>
+                            <?php endif; ?>
+                        </p>
+                        <p class="content">
+                        <?php echo $item['content']; ?>
+                        </p>
+                    </div>
+                </div>
+
+                <?php
+                }
+                ?>
+
+            </div>
+
+            <div class="flow_wrap month month02">
+
+            <?php 
+
+            $flow_items = array(
+                '12' => array(
+                    'title' => '上棟式（建前）',
+                    'orange' => '￥上棟後のお支払い',
+                    'content' => '家の骨組みを一気に建て上げる作業を｢上棟｣といいます。もともと工事の無事を祈る儀式でしたが、今では大工さんたちの労をねぎらう意味が強くなりました。この日までに窓の位置、高さを決定します。'
+                ),
+                '13' => array(
+                    'title' => '金物検査（瑕疵担保）',
+                    'content' => '構造用金物がしっかり入っているか、検査員がチェックします。'
+                ),
+                '14' => array(
+                    'title' => '断熱・外壁工事',
+                    'content' => '大切な外壁工事になります。これが貼られると家がより家らしくなりますよ。'
+                ),
+                '15' => array(
+                    'title' => '防水検査（瑕疵担保）',
+                    'content' => '外部から水が入ってこないか、防水の立ち上りなど自主検査をします。'
+                ),
+                '16' => array(
+                    'title' => '木炭敷き',
+                    'content' => '床下に木炭を吊るします。'
+                ),
+                '17' => array(
+                    'title' => '設備工事（電気・ガス・水道等）',
+                    'orange' => '￥中間金のお支払い',
+                    'content' => '一番大切なのはコンセントの位置と数、そして照明。実際の生活をイメージして考えるのは、なかなか難しいことですが、がんばりましょう。'
+                ),
+                '18' => array(
+                    'title' => '水周り機器の設置',
+                    'content' => 'キッチン・バス・トイレなどが運び込まれてきます。さあ、そろそろ内装を考えましょう。クロスの場合、種類が多いのでとても迷われる方が多いです。プレゼンボードを提出いたしますので、参考に色を決定していきます。'
+                ),
+                '19' => array(
+                    'title' => '内装工事',
+                    'content' => '工事は仕上げの段階に入っていきます。'
+                ),
+                '20' => array(
+                    'title' => '室内清掃',
+                    'content' => '施主様へお引渡しの前に、社内検査・クリーニングを行います'
+                ),
+                '21' => array(
+                    'title' => '完了検査',
+                    'content' => '図面通りの建物になっているか、検査員がきびしくチェックします。'
+                ),
+                '22' => array(
+                    'title' => 'お引渡し',
+                    'title' => '￥ご精算',
+                    'content' => '工事の内容が予定通りに行われているかを、ハマホームと施主様で確認します。このとき工事期間中に使用された鍵は破棄され、正規の鍵が施主様に渡されます。'
+                )
+                );
+             
+                foreach ($flow_items as $key => $item) {
+
+                ?>
+
+                <div class="flow_card">
+                    <div class="icon">
+                        <div class="icon_inner">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/flow/icon<?php echo $key; ?>.svg" alt="<?php echo $item['title']; ?>" width="80">
+                        </div>
+                    </div>
+                    <div class="text">
+                        <p class="title">
+                            <span class="number"><?php echo $key; ?></span>
+                            <?php echo $item['title']; 
+                            if(array_key_exists('orange', $item)) :
+                            ?>
+                            <span class="orange"><?php echo $item['orange']; ?></span>
+                            <?php endif; ?>
+                        </p>
+                        <p class="content">
+                        <?php echo $item['content']; ?>
+                        </p>
+                    </div>
+                </div>
+
+                <?php
+                }
+                ?>
+
+            </div>
+            <div class="flow_wrap">
+
+            <?php 
+
+            $flow_items = array(
+                '23' => array(
+                    'title' => 'お引越し',
+                    'content' => '施主様の方で、電話局、ケーブルテレビ、インターネット、火災保険等のお申し込みをお願いいたします。'
                 )
                 );
              
--- page-fuudo.php
+++ page-fuudo.php
@@ -34,16 +34,78 @@ $slug = $page->post_name;
             <div class="summer_wrap mt40">
 
             <p class="center">
-                
-            <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/img01.png" alt="丸太・梁材" width="550">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/img01.png" alt="夏の家" width="550">
             </p>
+
+            <!-- Slider main container -->
+            <div class="swiper swiper-summer mt40">
+            <!-- Additional required wrapper -->
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <div class="swiper_card">
+                        <p class="title">土壌木酢液散布・土台木酢液塗布</p>
+                        <div class="flex">
+                            <div class="col-lg6 center">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/swiper1.jpg" alt="土壌木酢液散布・土台木酢液塗布" width="200">
+                            </div>
+                            <div class="col-lg6">
+                                <p>
+                                シロアリ駆除には、有害物質石油製品を使わず自然素材の木酢液を使用。ハマホームでは基礎工事中に土壌に木酢液を散布。また、土台にも塗布しています。
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="swiper-slide">
+                    <div class="swiper_card">
+                        <p class="title">高機能断熱材</p>
+                        <div class="flex">
+                            <div class="col-lg6 center">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/swiper2.jpg" alt="高機能断熱材" width="200">
+                            </div>
+                            <div class="col-lg6">
+                                <p>
+                                ハマホームの断熱は、建物をすっぽり包む外断熱を採用。夏期、熱くなる屋根裏にも屋根直下断熱を施工します。断熱材には、高性能フェノールフォーム保温板「ネオマフォーム」を使っています。
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="swiper-slide">
+                    <div class="swiper_card">
+                        <p class="title">床下木炭</p>
+                        <div class="flex">
+                            <div class="col-lg6 center">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/swiper3.jpg" alt="床下木炭" width="200">
+                            </div>
+                            <div class="col-lg6">
+                                <p>
+                                床下で半永久的に休むことなく湿度を調整したり、悪臭、化学物質を吸着し、きれいな空気を出します。また、腐ることがありません。ハマホームでは床下に袋詰めにした炭を吊るします。
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- If we need pagination -->
+            <!-- <div class="swiper-pagination"></div> -->
+
+            <!-- If we need navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+
+            </div>
             </div>
 
             <div class="flex mt40">
                 <div class="col-lg6 flex">
                     <div class="col-sm3 col-lg4 mb20 center">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/summer01.png" alt="夏の気温" width="100">
-        </div>
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/summer01.png" alt="夏の気温" width="100">
+                    </div>
                     <div class="col-sm9 col-lg8 mb20">
                         <p>
                         福井県の夏の外気温とハマホームの家の気温と在来木造住宅の気温の変化を表したグラフです。外気温の低い6月は、室内温度が一般在来木造住宅よりも高く、外気温の高い7～8月は、室内温度が外気温よりも高い一般在来木造住宅と比べて、PS工法では外気温よりも低く住みやすい温度を保ちます。人にも家にも地球にも優しい家なのです。
@@ -52,14 +114,14 @@ $slug = $page->post_name;
                     <div class="col-12">
                         <p class="big center">
                         福井の夏の外気温とハマホームの家の気温の変化<br>
-            <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/summer03.jpg" alt="福井の夏の外気温とハマホームの家の気温の変化" width="330">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/summer03.jpg" alt="福井の夏の外気温とハマホームの家の気温の変化" width="380">
                         </p>
                     </div>
                 </div>
                 <div class="col-lg6 flex">
                     <div class="col-sm3 col-lg4 mb20 center">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/summer02.png" alt="夏の湿度" width="100">
-        </div>
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/summer02.png" alt="夏の湿度" width="100">
+                    </div>
                     <div class="col-sm9 col-lg8 mb20">
                         <p>
                         ハマホームの家では、6～8月を通して、外湿度が一般在来木造住宅より低くなっています。ハマホームの家では絶え上昇している空気が、熱を放出するだけでは なく湿 度 も外 へ 放 出しているからです。湿度が低いという事は、この時期に多い木の腐りやシロアリの発生が抑えられるという事です！つまり、健康長寿命の家という訳ですね！！
@@ -68,7 +130,126 @@ $slug = $page->post_name;
                     <div class="col-12">
                         <p class="big center">
                         福井の夏の外湿度とハマホームの家の湿度の変化<br>
-            <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/summer04.jpg" alt="福井の夏の外湿度とハマホームの家の湿度の変化" width="330">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/summer04.jpg" alt="福井の夏の外湿度とハマホームの家の湿度の変化" width="380">
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <h2>冬の家</h2>
+
+            <p class="big center">
+            福井の冬は『雪・湿気』とのたたかい
+            </p>
+
+            <p>
+            断熱材と、天井・内壁・床板の間に出来た通気路に、絶えず上昇気流を起こすハマホームの家では、暖房をつけた部屋の暖かさをこの通気路で家中に伝達します。また、夏は開放する屋根裏換気扇を、冬の間は閉じることで暖まった空気を外に逃がすことなく循環させます。だから暖房を入れた部屋と入れてない部屋の温度差の少ない、快適な環境になるんです。冬の朝、その暖かさを実感してください。そしてもう一つの大敵、湿気。昼夜の温度差が少ないハマホームの家は、湿度も一定に保ちます。詳しくは温度と湿度のグラフでご確認ください。
+            </p>
+
+            <div class="winter_wrap mt40">
+
+            <p class="center">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/img02.png" alt="冬の家" width="550">
+            </p>
+
+            <!-- Slider main container -->
+            <div class="swiper swiper-winter mt40">
+            <!-- Additional required wrapper -->
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <div class="swiper_card">
+                        <p class="title">土壌木酢液散布・土台木酢液塗布</p>
+                        <div class="flex">
+                            <div class="col-lg6 center">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/swiper1.jpg" alt="土壌木酢液散布・土台木酢液塗布" width="200">
+                            </div>
+                            <div class="col-lg6">
+                                <p>
+                                シロアリ駆除には、有害物質石油製品を使わず自然素材の木酢液を使用。ハマホームでは基礎工事中に土壌に木酢液を散布。また、土台にも塗布しています。
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="swiper-slide">
+                    <div class="swiper_card">
+                        <p class="title">高機能断熱材</p>
+                        <div class="flex">
+                            <div class="col-lg6 center">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/swiper2.jpg" alt="高機能断熱材" width="200">
+                            </div>
+                            <div class="col-lg6">
+                                <p>
+                                ハマホームの断熱は、建物をすっぽり包む外断熱を採用。夏期、熱くなる屋根裏にも屋根直下断熱を施工します。断熱材には、高性能フェノールフォーム保温板「ネオマフォーム」を使っています。
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="swiper-slide">
+                    <div class="swiper_card">
+                        <p class="title">床下木炭</p>
+                        <div class="flex">
+                            <div class="col-lg6 center">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/swiper3.jpg" alt="床下木炭" width="200">
+                            </div>
+                            <div class="col-lg6">
+                                <p>
+                                床下で半永久的に休むことなく湿度を調整したり、悪臭、化学物質を吸着し、きれいな空気を出します。また、腐ることがありません。ハマホームでは床下に袋詰めにした炭を吊るします。
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- If we need pagination -->
+            <!-- <div class="swiper-pagination"></div> -->
+
+            <!-- If we need navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+
+            </div>
+            </div>
+
+            <div class="flex mt40">
+                <div class="col-lg6 flex">
+                    <div class="col-sm3 col-lg4 mb20 center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/winter01.png" alt="冬の気温" width="100">
+                    </div>
+                    <div class="col-sm9 col-lg8 mb20">
+                        <p>
+                        福井県の冬の外気温とハマホームの家の気温と在来木造住宅の気温の変化を表したグラフです。PS工法と一般在来木造住宅との室内温度差は……見ての通り歴然です！　外気温と共に下がる一般在来木造住宅に比べ、PS工法は、室内の熱を外にださないので20度前後を保っています。暖房のない部屋や冬の早朝でも一定の気温を保つハマホームの家は、年配の方やお子様にも安心な家なのです。
+                        </p>
+                    </div>
+                    <div class="col-12">
+                        <p class="big center">
+                        福井の冬の外気温とハマホームの家の気温の変化<br>
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/winter03.jpg" alt="福井の冬の外気温とハマホームの家の気温の変化" width="380">
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg6 flex">
+                    <div class="col-sm3 col-lg4 mb20 center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/winter02.png" alt="夏の湿度" width="100">
+                    </div>
+                    <div class="col-sm9 col-lg8 mb20">
+                        <p>
+                        冬の室内は、洗濯物を干したり、ストーブをつけたりで湿度も高くなりがちです。しかしPS工法では、家の中の湿度は50％前後！　この湿度は人にとって一番過ごしやすい湿度なんです。もちろん家にとってもいい環境です！　ハマホームの家は、温度も湿度も快適に保ってくれます。
+                        </p>
+                    </div>
+                    <div class="col-12">
+                        <p class="big center">
+                        福井の冬の外湿度とハマホームの家の湿度の変化<br>
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/fuudo/winter04.jpg" alt="福井の冬の外湿度とハマホームの家の湿度の変化" width="380">
                         </p>
                     </div>
                 </div>
