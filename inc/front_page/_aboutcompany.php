6399e69f9ba815a7c83a7411503eac8a8e746e06
gtgtgl <ykr20141101@outlook.jp>
2022-05-30 10:28:20 +0900

    仮テキスト等修正

--- inc/front_page/_aboutcompany.php
+++ inc/front_page/_aboutcompany.php
@@ -17,7 +17,7 @@
                 </div>
 
                 <div class="col-sm8 col-md12">
-                    <a href="#" class="banner02">
+                    <a class="banner02">
                         <div class="bg_screen"></div>
                         <img src="<?php echo get_template_directory_uri(); ?>/img/bg03.jpg" alt="コワーキングスペースを増設しました" class="bgimg">
                         <div class="text">
@@ -25,7 +25,7 @@
                                 コワーキングスペースを<br>増設しました！
                             </p>
                             <p>
-                            テキストが入ります。テキストが入ります。テキストが入りますテキストが入ります。テキストが入ります。テキストが入ります。
+                            小さな会議やお集まりにご利用ください。
                             </p>
                         </div>
                     </a>
--- inc/front_page/_photo.php
+++ inc/front_page/_photo.php
@@ -3,7 +3,7 @@
         <h2>目で<span class="orange">見る</span>写真</h2>
 
         <p class="left-center">
-        テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+        Instagramでも、当社が施工した物件を写真で見ることができます。
         </p>
 
         <a href="https://www.instagram.com/hamahome.fukui/?hl=ja" class="btn-01" target="_blank">Instagram</a>
